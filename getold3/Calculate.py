bf5009105092bfcd9f501f23736b44f31bce5a91
poxnora <kwojcik99@gmail.com>
2022-06-17 15:42:56 +0200

    New calculate method

--- getold3/Calculate.py
+++ getold3/Calculate.py
@@ -2,11 +2,14 @@ import pandas as pd
 
 
 class Calculate:
-    doc = ""
-    n = 0
 
     def __init__(self, doc, n):
         self.doc = doc
+        self.doc_time = doc
+        self.doc_time['Datetime'] = pd.to_datetime(self.doc['Datetime'], errors='coerce')
+        self.dates = doc['Datetime'].dt.strftime('%Y-%m-%d')
+        self.doc_time['Datetime'] = self.dates
+        self.dates_set = (x for x in self.dates)
         self.n = n
 
     def retweet(self):
@@ -22,28 +25,37 @@ class Calculate:
         return self.doc.nsmallest(n=self.n, columns=['Label']).index.values
 
     def each_day(self):
-        self.doc['Datetime'] = pd.to_datetime(self.doc['Datetime'], errors='coerce')
-        dates = self.doc['Datetime'].dt.strftime('%Y-%m-%d')
         value_map = {}
-        for date in dates:
+        for date in self.dates:
             value = value_map.get(date)
             if value is None:
                 value_map.update({date: 1})
             else:
                 value += 1
                 value_map.update({date: value})
-        print(value_map)
         return value_map
 
     def each_day_sentiment(self):
-        self.doc['Datetime'] = pd.to_datetime(self.doc['Datetime'], errors='coerce')
-        dates = self.doc['Datetime'].dt.strftime('%Y-%m-%d')
-        self.doc['Datetime'] = dates
         value_map = {}
-        dates_set = (x for x in dates)
-        for date in dates_set:
-            r = self.doc[self.doc['Datetime'] == date]
+        for date in self.dates_set:
+            r = self.doc_time[self.doc_time['Datetime'] == date]
             mean = r['Label'].mean()
             if mean != 0:
                 value_map.update({date: mean})
         return value_map
+
+    def last_n_sentiment(self,n):
+        ranges = 0
+        mean = []
+        true_mean = 0
+        for date in self.dates_set:
+            ranges += 1
+            if ranges < n:
+                r = self.doc_time[self.doc_time['Datetime'] == date]
+                mean.append(r['Label'].mean())
+            else:
+                break
+        for number in mean:
+            true_mean += number
+        return true_mean / len(mean)
+
